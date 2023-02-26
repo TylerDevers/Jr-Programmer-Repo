33c271ca2929fcbd13a5ce4a8a16d187a1d73687
Tyler Devers <TylerDevers@users.noreply.github.com>
2023-02-26 11:13:36 -0800

    update Menu script
    add start and exit functionality to menu scene

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -21,4 +22,38 @@ public class MenuUIHandler : MonoBehaviour
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
     }
+
+    public void StartNew() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit() {
+        // the below method only works in the built version, not play testing.
+        Application.Quit();
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
