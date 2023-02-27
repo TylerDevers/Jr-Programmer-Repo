0edfc4782861369d6918a1941255eb2d14d5e3ff
Tyler Devers <TylerDevers@users.noreply.github.com>
2023-02-27 15:11:09 -0800

    add MainManager
    make game settings persist between scenes and game sessions.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -16,7 +16,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
     
     private void Start()
@@ -24,6 +24,8 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew() {
@@ -37,9 +39,20 @@ public class MenuUIHandler : MonoBehaviour
         #else
                 Application.Quit(); // original code to quit Unity player
         #endif
+
+        MainManager.Instance.SaveColor(); 
     }
 
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
 
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
 
 
 
